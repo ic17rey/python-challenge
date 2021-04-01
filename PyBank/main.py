506c4caee48c835910319a4404702eecaac2e52a
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-03-31 23:02:20 -0400

    Create a list of profits/losses for analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,7 @@
     # output csv file to include:
     # total number of months in the dataset
     # net total of "Profit/Losses" over the time period
+    # calculate changes in profit/loss and find the average of those changes
     # date and amount of greatest increase in profits
     # date and amount of the greatest decrease
     # print analysis to the terminal and export to the output csv file
@@ -20,18 +21,32 @@ csv_path = os.path.join('.', 'Resources', 'budget_data.csv')
 with open(csv_path) as filehandler:
 
     csv_reader = csv.reader(filehandler, delimiter=',')
-
-    #move to the row beneath the header to begin analysis of data
-   
-    months = csv_reader.line_num
+    
     csv_header = next(csv_reader)
-    
-    #print(csv_header)
-    
+
+    profits = 0
+
     for row in csv_reader:
         
-        #test the file path to budget_data.csv
-        #print(row[0])
-        months += 1
-    
+    # to grab the count of rows in the csv file (subtract 1 for the header row)
+        months = csv_reader.line_num - 1
+        
+        profits += int(row[1])
+           
     print("Total months: " + str(months))
+    print("Total Profit/Losses: $" + str(profits))
+    
+with open(csv_path) as filehandler:
+  
+    csv_reader = csv.reader(filehandler, delimiter=',')
+    
+    # move to the row beneath the header to begin analysis of data
+    csv_header = next(csv_reader)
+    
+    # create a list from the csv file to analyze the profits/losses
+    profits_list = []
+       
+    for row in csv_reader:
+        print(row[1])
+        profits_list.append(row[1])       
+        
