f5a1463c730d8361729ddb2f107ad8de25b02b14
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-04-02 23:57:16 -0400

    Import modules, include path to source file and begin calculations

---
+++ PyPoll/main.py
@@ -0,0 +1,54 @@
+#==================
+# PyPoll Challenge - create analysis of election data that exports to a csv file
+#==================
+    
+    # csv file output and analysis printed to terminal to include:
+    # total number of votes cast and list of candidates who received votes
+    # percentage of votes and number of votes each candidate won 
+    # winner of election from the popular vote
+   
+# begin with import of os and csv modules
+import os
+import csv
+
+# identify the path to the resource file stored in PyBank
+csv_path = os.path.join('.', 'Resources', 'election_data.csv')
+
+# use the csv module to read the file
+with open(csv_path) as file_handler:
+
+    csv_reader = csv.reader(file_handler, delimiter=',')
+
+    # move to the row beneath the header to begin analysis of data
+    csv_header = next(csv_reader)
+
+    # create a list to be used to analyze election_data.csv contents
+    votes_list = []
+
+    for row in csv_reader:
+        
+        votes_list.append(row[0])
+        
+    print("Total Votes: " + str(len(votes_list)))
+
+# use the csv module to read the file
+# start a dictionary for votes_list
+votes_cast={}
+
+with open(csv_path) as file_handler:
+
+    csv_reader = csv.reader(file_handler, delimiter=',')
+
+    # move to the row beneath the header to begin analysis of data
+    csv_header = next(csv_reader)
+
+    for row in csv_reader:
+        if row[2] not in votes_cast
+            votes_cast[row[2]] = 0
+        votes_cast[row[2]] += 1
+
+print(vote_cast)
+
+
+
+    
