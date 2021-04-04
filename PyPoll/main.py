333e96d7a6682452d95aab026909aeee06f00b4e
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-04-03 22:58:04 -0400

    Syntax correction

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -32,6 +32,24 @@ with open(csv_path) as file_handler:
     output = f'Election Results\n-----------------\n'
     output += f"Total Votes: {(len(votes_list))}\n"
 
+
+# want to start a dictionary for votes_cast
+
+votes_cast = {}
+
+with open(csv_path) as file_handler:
+
+    csv_reader = csv.reader(file_handler, delimiter=',')
+
+    # move to the row beneath the header to begin analysis of data
+    csv_header = next(csv_reader)
+
+    for row in csv_reader:
+        if row[2] not in votes_cast:
+            votes_cast[row[2]] = 0
+        votes_cast[row[2]] += 1
+
+
 print(output)
 
 # write output lines to csv in Analysis folder
@@ -40,25 +58,6 @@ output_path = os.path.join("Analysis", "analysis.csv")
 with open(output_path, 'w', newline='') as data_file:
 
     data_file.write(output)
-
-# use the csv module to read the file
-# start a dictionary for votes_list
-
-# votes_cast={}
-
-# with open(csv_path) as file_handler:
-
-#     csv_reader = csv.reader(file_handler, delimiter=',')
-
-#     # move to the row beneath the header to begin analysis of data
-#     csv_header = next(csv_reader)
-
-#     for row in csv_reader:
-#         if row[2] not in votes_cast
-#             votes_cast[row[2]] = 0
-#         votes_cast[row[2]] += 1
-
-# print(vote_cast)
 
 
 
