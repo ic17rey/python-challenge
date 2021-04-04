4cdcce4f16929267517feffc24d324ff543704be
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-04-03 22:49:43 -0400

    Finish clean up of comments and submit challenge for review

--- PyBank/main.py
+++ PyBank/main.py
@@ -23,7 +23,7 @@ with open(csv_path) as file_handler:
     
     # move to the row beneath the header to begin analysis of data
     csv_header = next(csv_reader)
-    #first_row
+    
     # create a list from the csv file data to analyze the profits/losses
     profits_list = []
     
@@ -32,11 +32,9 @@ with open(csv_path) as file_handler:
         
         # find starting value for profits
         profit_month1 = profits_list[0]
+        
         # find final value for profits (last row profits value)
         final_profits = profits_list[-1]
-    
-    #print(profit_month1)
-    #print(final_profits)
     
     # find overall profit or loss over the months in the data set
     total = int(final_profits) - int(profit_month1) 
@@ -44,10 +42,9 @@ with open(csv_path) as file_handler:
     # find the number of months involved
     total_months = len(profits_list)
 
-    #print(total_months)
-    #prints above all hold the correct values
+    # print tests all held the correct values for above variables
     
-    # calculate the incremental changes in profit value to find greatest increase and greatest decrease
+# calculate the incremental changes in profit value to find greatest increase and greatest decrease
 # temporarily put the data from the file to a variable file_handler
 with open(csv_path) as file_handler:
   
@@ -56,6 +53,8 @@ with open(csv_path) as file_handler:
     
     # move to the row beneath the header to begin analysis of data
     csv_header = next(csv_reader)
+    
+    # begin initializing variables and set to 0
     months = 0
     total_profit = 0
     
@@ -66,36 +65,51 @@ with open(csv_path) as file_handler:
     average_change = 0
     round_average = 0
     max_profit_increase = 0
+    month_max_profit = 0
     max_loss_decrease = 0
+    month_max_loss_decrease = 0
+    
+    # for finding the month where max)profit)increae and max_loss_decrease occur, create a string variable
+    month_of_interest = ""
 
     # create a list to hold the changes in profits/losses
     change_in_profit_list = []
-    # print prev_profit, to ensure equal to 867884 (first month profit)
+    month_of_interest_list = []
     
     for row in csv_reader:
-        #determine the change in the profit
+    
+        # add to months as loop runs
         months += 1
+
+        # identify the column that holds the profit/loss value
         current_profit = int(row[1])
         
         # as going through the loop sum up total profits/losses 
         total_profit += int(row[1])
 
+        # identify the month for the current loop
+        month_of_interest = (row[0])
+
         # as loop progresses each change is profit for that row minus the previous row's profit
         change_in_profit = current_profit - prev_profit
-        change_in_profit_list.append(change_in_profit)
         
+        # add values for changes in profit to the list
+        change_in_profit_list.append(change_in_profit)
+
+        # need to find what month the max and min occur in 
+        month_of_interest_list.append(month_of_interest)
         # reassign the value of current profit to previous profit in advance of next loop
         prev_profit = current_profit
     
+    # use max and min functions to identify profit change values of interest
     max_profit_increase = max(change_in_profit_list)
     max_loss_decrease = min(change_in_profit_list)
     
-    # need to find what month the max and min occur in
-    
+    # identify locations for the max and min values of interest
+
     # calculate the average change and round to two decimal places
     average_change = round(total / (total_months - 1), 2)
-    #print(average_change)
-
+    
     #create a variable for output, that will be printed to terminal and exported to a file
     output = f'Financial Analysis\n------------------\n'    
     
